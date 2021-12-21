8245cca522327cee33732024b072ae352a32f323
blackPrincess31 <katesavchenko31@gmail.com>
2021-12-21 20:41:02 +0300

    task 4js done

--- task4.js
+++ task4.js
@@ -1,12 +1,10 @@
+
 let randomatic = require('randomatic');
-let crypto = require('crypto')
+let crypto = require('crypto');
+const { argv } = require('process');
 
 
 let menu_items = "1 - rock\n2 - paper\n3 - scissors\n4 - lizard\n5 - Spock\n0 - exit\n? - help\n Enter your move:";
-
-let mid_value = 3;
-
-
 let err_prompt_message = "Wrong input";
 
 var readline = require('readline').createInterface({
@@ -22,6 +20,21 @@ let choose_table = {
     5: 'spock',
 };
 
+
+/*class arguments{
+
+    constructor(){
+        
+    }
+    }
+     this.process = require('process'); 
+     if (process.argv.length >= 3) {
+             for(i=process.argv[3];i<=process.argv[process.argv.length];i++){
+             value = process.argv[i]; 
+             ProcessGameInput(value);
+   
+   
+}*/
 
 class HmacKey {
     value;
@@ -46,8 +59,8 @@ class Computer {
     hmac;
     #max = 5;
     #min = 1;
-    
-    constructor(){
+
+    constructor() {
         this.MakeChoise();
     }
     MakeChoise() {
@@ -60,10 +73,11 @@ computer_player = new Computer();
 
 class Game {
     //returns -1 for left won, 1 for right won or 0 for draw
+    static #mid_value = 3;
     static Compare(left, right) {
         let diff = left - right;
 
-        if (Math.abs(diff) >= mid_value) {
+        if (Math.abs(diff) >= this.#mid_value) {
             if (diff > 0) {
                 return 1;
             }
@@ -88,13 +102,13 @@ class TabHelp {
         '-1': 'Win',
         '0': 'Draw'
     };
-    
+
     CallHelp() {
         let formated_table = {};
         for (const left in choose_table) {
             formated_table[choose_table[left]] = {}
             for (const right in choose_table) {
-                let result = CompareChoice(left, right);
+                let result = Game.Compare(left, right);
                 formated_table[choose_table[left]][choose_table[right]] = this.#help_result_table[result];
             }
         }
@@ -107,7 +121,7 @@ class MessageProvider {
     static #user_win_message = "You win";
     static #pc_win_message = "Computer Win";
     static #draw_message = "Draw";
-    
+
     static #help = new TabHelp();
     static PrintWinner(user, pc) {
         let result = Game.Compare(user, pc)
@@ -118,33 +132,33 @@ class MessageProvider {
         if (result == 1) {
             console.log(this.#pc_win_message);
         }
-        if (result == 0){
+        if (result == 0) {
             console.log(this.#draw_message);
         }
-        
+
     }
-    
-    static PrintHelp(){
+
+    static PrintHelp() {
         this.#help.CallHelp();
     }
 
-    static PrintMenu(){
+    static PrintMenu() {
         console.log(menu_items);
     }
 
-    static PrintError(){
+    static PrintError() {
         console.log(err_prompt_message)
     }
 
-    static PrintComputerHMAC(){
+    static PrintComputerHMAC() {
         console.log("Compuuter HMAC: " + computer_player.hmac.value);
     }
 
-    static PrintComputerKey(){
+    static PrintComputerKey() {
         console.log("Computer Key: " + computer_player.hmac.key.value);
     }
 
-    static PrintGameResult(computer_step, user_choose){
+    static PrintGameResult(computer_step, user_choose) {
         console.log('Computer move: ' + choose_table[computer_step]);
         console.log('Your move: ' + choose_table[user_choose]);
 
@@ -152,8 +166,8 @@ class MessageProvider {
         this.PrintWinner(user_choose, computer_step);
         this.PrintComputerKey();
     }
-    
-    static PrintGameStart(){
+
+    static PrintGameStart() {
         console.log('--Task 4--');
 
         computer_player.MakeChoise();
@@ -162,7 +176,10 @@ class MessageProvider {
         this.PrintMenu();
     }
 
-    static ProcessGameInput(user_input){
+    static ProcessGameInput(user_input) {
+
+        this.process = require('process');
+
         if (user_input == '0') {
             readline.close();
             return
@@ -171,7 +188,7 @@ class MessageProvider {
             MessageProvider.PrintHelp()
             return
         }
-    
+
         let user_choose = '';
         for (const el in choose_table) {
             if (user_input == el || user_input == choose_table[el]) {
@@ -179,25 +196,38 @@ class MessageProvider {
                 break;
             }
         }
-    
+
         if (user_choose == '') {
             MessageProvider.PrintError()
             return
         }
 
         this.PrintGameResult(computer_player.choise, user_choose);
+        computer_player.MakeChoise();
     }
+
 }
 
-readline.on('line', (user_input) => {
-    MessageProvider.ProcessGameInput(user_input);
 
-    console.log('------------');
 
-    computer_player.MakeChoise();
-    
-    MessageProvider.PrintComputerHMAC();
-    MessageProvider.PrintMenu();
-});
+if (process.argv.length >= 4) {
+    for (let i = 2; i < process.argv.length; i++) {
+        console.log(" Game #", i - 1)
 
-MessageProvider.PrintGameStart();
+        MessageProvider.PrintComputerHMAC();
+        MessageProvider.ProcessGameInput(process.argv[i]);
+    }
+    readline.close();
+} else {
+    readline.on('line', (user_input) => {
+        MessageProvider.ProcessGameInput(user_input);
+
+        console.log('------------');
+
+        MessageProvider.PrintComputerHMAC();
+        MessageProvider.PrintMenu();
+
+    });
+    MessageProvider.PrintGameStart()
+}
+
